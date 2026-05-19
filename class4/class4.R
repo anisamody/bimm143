3fd5ed0e74962e4290a14c48c2ad28d9a64f30d2
Anisa Mody <anisa@am-mac-1.local>
2026-05-19 12:46:25 -0700

    Add class4 to to class7

---
+++ class4/class4.R
@@ -0,0 +1,10 @@
+#' ---
+#' title: "Class 4: Introduction to R"
+#' author: "Anisa Mody (PID: A19145291)"
+#' date: "April 9th, 2026"
+#' ---
+
+# text file
+x <- 1:50
+plot(x, sin(x), type = "l", col = "pink", lwd = 3)
+
